3582eee6fbc02830b7517ca8c8c2742a7364bbb0
samosa7 <71269976+samosa7@users.noreply.github.com>
2022-05-19 03:53:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route("/")
-def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework!"
+def hello():
+    return " "
 
+@app.route('/stfuanu.html')
+def index():
+   return "stfuanu sub-tko-poc\n"
+
+if __name__ == '__main__':
+   app.run()
